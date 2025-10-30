cddfd8179782e92f3366e70d9e059bf7a520ee89
Liza Mardiana <lizamardiana16@gmail.com>
2025-10-31 02:03:21 +0700

    newww

--- resources/views/laporan/index.blade.php
+++ resources/views/laporan/index.blade.php
@@ -12,7 +12,7 @@
     <div class="col-12">
         <div class="card">
             <div class="card-header">
-                <h5 class="card-title mb-0">Generate Laporan</h5>
+                <h5 class="card-title mb-0">Buat Laporan</h5>
             </div>
             <div class="card-body">
                 <form action="{{ route('laporan.generate') }}" method="GET" class="needs-validation" novalidate>
